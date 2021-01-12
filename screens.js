6dcd632f6405d08db6dd3bb9c2cdbdac938362e4
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2021-01-12 14:29:05 +0200

    added BGBlurredCarousel

--- screens.js
+++ screens.js
@@ -3,11 +3,13 @@ import ProgressBar from './src/screens/ProgressBar'
 import TopTabs from './src/screens/TopTabs'
 import AnalogClock from './src/screens/AnalogClock'
 import DonutChart from './src/screens/DonutChart'
+import BGBlurredCarousel from './src/screens/BGBlurredCarousel'
 
 export default SCREENS = [
-    { name: 'morph-rating', component: MorphRating },
-    { name: 'progress-bar', component: ProgressBar },
-    { name: 'top-tabs', component: TopTabs },
-    { name: 'analog-clock', component: AnalogClock },
-    { name: 'donut-chart', component: DonutChart }
+  { name: 'morph-rating', component: MorphRating },
+  { name: 'progress-bar', component: ProgressBar },
+  { name: 'top-tabs', component: TopTabs },
+  { name: 'analog-clock', component: AnalogClock },
+  { name: 'donut-chart', component: DonutChart },
+  { name: 'bg-blurred-carousel', component: BGBlurredCarousel }
 ]
